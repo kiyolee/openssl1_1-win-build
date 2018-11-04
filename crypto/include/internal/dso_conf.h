ff7645d47699b0b6bc16237074ad09ced5c03417
Kelvin Lee <kiyolee@gmail.com>
2018-11-04 23:40:04 +1100

    Add VS2017 projects/solution. [Work in progress]

---
+++ crypto/include/internal/dso_conf.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2016-2018 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the OpenSSL license (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+/*
+ * Derived from dso_conf.h.in for https://github.com/kiyolee/openssl1_1-win-build.
+ */
+
+#ifndef HEADER_DSO_CONF_H
+# define HEADER_DSO_CONF_H
+
+# define DSO_WIN32
+# define DSO_EXTENSION ""
+
+#endif
