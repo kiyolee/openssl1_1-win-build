3c45cc64a19ec6db6c1afebe77678dbaa7044fd5
Kelvin Lee <kiyolee@gmail.com>
2020-01-07 11:16:04 +1100

    Remove empty directories after configure.

--- distfiles/config_all.cmd
+++ distfiles/config_all.cmd
@@ -17,21 +17,25 @@ mkdir lib32
 pushd dll64
 perl %OPENSSL_DIR%\Configure --prefix="%ProgramFiles%\OpenSSL-1_1" --with-zlib-include=%ZLIB_DIR% --with-zlib-lib=%ZLIB_DIR%\build\x64\Release\libz-static.lib VC-WIN64A-masm no-dynamic-engine zlib
 call :genfile
+call :clndir
 popd
 
 pushd lib64
 perl %OPENSSL_DIR%\Configure --prefix="%ProgramFiles%\OpenSSL-1_1" --with-zlib-include=%ZLIB_DIR% --with-zlib-lib=%ZLIB_DIR%\build\x64\Release\libz-static.lib VC-WIN64A-masm no-shared no-dynamic-engine zlib
 call :genfile
+call :clndir
 popd
 
 pushd dll32
 perl %OPENSSL_DIR%\Configure --prefix="%ProgramFiles(x86)%\OpenSSL-1_1" --with-zlib-include=%ZLIB_DIR% --with-zlib-lib=%ZLIB_DIR%\build\Release\libz-static.lib VC-WIN32 no-dynamic-engine zlib
 call :genfile
+call :clndir
 popd
 
 pushd lib32
 perl %OPENSSL_DIR%\Configure --prefix="%ProgramFiles(x86)%\OpenSSL-1_1" --with-zlib-include=%ZLIB_DIR% --with-zlib-lib=%ZLIB_DIR%\build\Release\libz-static.lib VC-WIN32 no-shared no-dynamic-engine zlib
 call :genfile
+call :clndir
 popd
 
 goto :end
@@ -46,6 +50,21 @@ perl -I. -Mconfigdata %OPENSSL_DIR%\util\dofile.pl -omakefile %OPENSSL_DIR%\tool
 ren configdata.pm configdata.pm.org
 sed -e "s/%OPENSSL_DIR_SED%/\./g" configdata.pm.org > configdata.pm
 dos2unix bn_conf.h dso_conf.h opensslconf.h apps\CA.pl apps\tsget.pl tools\c_rehash.pl
+exit /b
+
+:clndir
+@echo off
+call :clndir0
+@echo on
+exit /b
+
+:clndir0
+for /d %%d in ( * ) do (
+    pushd %%d
+    call :clndir0
+    popd
+    rmdir %%d 2>nul
+)
 exit /b
 
 :end
