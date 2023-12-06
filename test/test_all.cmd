a6315c3ca97a081edbc1bce5b6518aeed7c0d965
Kelvin Lee <kiyolee@gmail.com>
2023-12-06 23:02:59 +1100

    Improve test_all.cmd

--- test/test_all.cmd
+++ test/test_all.cmd
@@ -7,15 +7,25 @@ set MY_ROOT=%cd%
 echo ROOT: %MY_ROOT%
 echo.
 
-for /d %%d in ( build-* ) do (
-    for %%r in ( Release Debug x64\Release x64\Debug ) do (
-        if exist %MY_ROOT%\%%d\%%r\openssl.exe (
-            call test\test_one.cmd %%d\%%r
-            echo.
-        )
-        if exist %MY_ROOT%\%%d\%%r\openssl-static.exe (
-            call test\test_one.cmd %%d\%%r -static
-            echo.
+if .%1. == .. goto :all
+set TEST_BUILDS=%*
+goto :start
+:all
+set TEST_BUILDS=build-VS*
+goto :start
+
+:start
+for /d %%d in ( %TEST_BUILDS% ) do (
+    if exist %%d\ (
+        for %%r in ( Release Debug x64\Release x64\Debug ) do (
+            if exist %MY_ROOT%\%%d\%%r\openssl.exe (
+                call test\test_one.cmd %%d\%%r
+                echo.
+            )
+            if exist %MY_ROOT%\%%d\%%r\openssl-static.exe (
+                call test\test_one.cmd %%d\%%r -static
+                echo.
+            )
         )
     )
 )
